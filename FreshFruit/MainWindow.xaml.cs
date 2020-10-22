c41abe2c6adbd81bd7eaa6d2d5b37482601b042a
Raihante <raehancyber@gmail.com>
2020-10-23 02:34:47 +0700

    Add project files.

---
+++ FreshFruit/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FreshFruit.Model;
+using FreshFruit.Controller;
+
+
+namespace FreshFruit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, BucketEventListener
+    {
+
+        private Seller reihan;
+        Fruit anggur = new Fruit("Anggur");
+        Fruit Apel = new Fruit("Apel");
+        Fruit Pisang = new Fruit("Pisang");
+        Fruit jeruk = new Fruit("Jeruk");
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bucket keranjangBuah = new Bucket(4); // bisa diisi maksimal keranjang
+            BucketController bucketController = new
+            BucketController(keranjangBuah, this);
+            reihan = new Seller("reihan", bucketController);
+            ListBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            reihan.addFruit(anggur);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            reihan.addFruit(Apel);
+
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            reihan.addFruit(Pisang);
+
+        }
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            reihan.addFruit(jeruk);
+
+        }
+        private void removerei_Click(object sender, RoutedEventArgs e)
+        {
+            reihan.rmvFruitreiihunn(anggur);
+            reihan.rmvFruitreiihunn(Apel);
+            reihan.rmvFruitreiihunn(Pisang);
+            reihan.rmvFruitreiihunn(jeruk);
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        public void onSucceed(string message)
+        {
+            MessageBox.Show(message, "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            ListBoxBucket.Items.Refresh();
+        }
+
+    }
+
+}
---
+++ FreshFruit/readme.md
@@ -0,0 +1,13 @@
+
+# Fresh Fruit + Delete Function
+
+## Description
+Program Fresh Fruit ini bertujuan menambahkan item ke sebuah keranjang digital
+## Scope of Functions
+
+Berikut beberapa fungsi dari projek ini
+
+- Menambahkan barang sampai 4 buah
+- Penambahan fungsi delete pada program
+- Penambahan messagebox pada saat menambahkan dan menghapus
+- Ditambahkan messagebox image warning dan information
